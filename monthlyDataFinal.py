ef61be9af950073ecf2327fe6b6af0dc1a2a13be
michael-hgr <michaelhager39@gmail.com>
2025-02-06 15:29:22 +0100

    Add files via upload

---
+++ monthlyDataFinal.py
@@ -0,0 +1,55 @@
+import requests
+import pandas as pd
+
+
+def monthlyData(latitude, longitude, peakpower, loss, angle, aspect, mountingplace, startyear=2022, endyear=2023):
+    url = "https://re.jrc.ec.europa.eu/api/seriescalc"
+
+    params = {
+        "lat": latitude,
+        "lon": longitude,
+        "usehorizon": 1,
+        "pvcalculation": 1,
+        "mountingplace": mountingplace,
+        "peakpower": peakpower,
+        "loss": loss,
+        "angle": angle,
+        "aspect": aspect,
+        "outputformat": "json"
+    }
+
+    optimalParams = params.copy()
+    optimalParams.update({
+        "startyear": startyear,
+        "endyear": endyear,
+        "optimalangles": 1
+    })
+
+    response = requests.get(url, params=params)
+    optimalResponse = requests.get(url, params=optimalParams)
+
+    if response.status_code == 200:
+        data = response.json()
+        optimalData = optimalResponse.json()
+
+        optimal_slope = optimalData["inputs"]["mounting_system"]["fixed"]["slope"]["value"]
+        optimal_azimuth = optimalData["inputs"]["mounting_system"]["fixed"]["azimuth"]["value"]
+
+        print(f"Optimaler Winkel: {optimal_slope}°")
+        print(f"Optimaler Azimut: {optimal_azimuth}°")
+
+        hourly_data = data.get("outputs", {}).get("hourly", [])
+        if not hourly_data:
+            print("Keine stündlichen Daten gefunden.")
+        else:
+            df = pd.DataFrame(hourly_data)
+            df['time'] = pd.to_datetime(df['time'], format='%Y%m%d:%H%M')
+            df['Energie_kWh'] = df['P'] / 1000
+            df['Energie_kWh'] = df['Energie_kWh'].clip(lower=0)
+            df['YearMonth'] = df['time'].dt.to_period('M')
+            monthly_energy = df.groupby('YearMonth')['Energie_kWh'].sum().reset_index()
+            monthly_energy.rename(columns={"YearMonth": "ds", "Energie_kWh": "y"}, inplace=True)
+            monthly_energy['ds'] = pd.to_datetime(monthly_energy['ds'].astype(str))
+            return monthly_energy
+    else:
+        raise ConnectionError(f"Fehler bei der Anfrage. Status Code: {response.status_code}")
---
+++ prognose.py
@@ -0,0 +1,26 @@
+from prophet import Prophet
+import pandas as pd
+from monthlyDataFinal import monthlyData
+
+
+def prognoseMonthlyData(latitude, longitude, peakpower, loss, angle, aspect, mountingplace):
+    monthly_energy = monthlyData(latitude, longitude, peakpower, loss, angle, aspect, mountingplace)
+
+    model = Prophet(yearly_seasonality=True, weekly_seasonality=False, growth='flat', changepoint_prior_scale=0.1)
+    model.add_seasonality(name='monthly', period=30.5, fourier_order=2)
+    model.add_seasonality(name='custom_yearly', period=12, fourier_order=2)
+    
+    model.fit(monthly_energy)
+
+    future = model.make_future_dataframe(periods=120, freq='MS')
+    future = future[future['ds'].dt.year >= 2024]
+    
+    forecast = model.predict(future)
+    forecast['yhat'] = forecast['yhat'].clip(lower=0)
+    forecast['yhat_lower'] = forecast['yhat_lower'].clip(lower=0)
+    forecast['yhat_upper'] = forecast['yhat_upper'].clip(lower=0)
+
+    prognoseResult = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
+
+    
+    return prognoseResult.to_dict(orient='records')
